dfeb1e1fcfafc669cd9a11eb39b5614e25b75e03
Waldener Monteiro <jjrr019@gmail.com>
2021-02-07 23:46:41 -0300

    refactor: utilizing the AuthProvider with share all auth options

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {AuthProvider} from './contexts/auth';
 import Routes from './routes';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </NavigationContainer>
   );
 };
