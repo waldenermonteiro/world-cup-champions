eaa7264069d1e96706c81709ca6fd362a0515b2f
Waldener Monteiro <jjrr019@gmail.com>
2021-02-07 23:03:51 -0300

    feature: move App.js for src and create navigation with routes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import Routes from './routes';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  );
+};
+
+export default App;
