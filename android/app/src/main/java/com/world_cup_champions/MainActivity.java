8b36cc7b66239d9d073f12b45f58a3401f6591c7
Waldener Monteiro <jjrr019@gmail.com>
2021-02-07 22:28:04 -0300

    initial commit

---
+++ android/app/src/main/java/com/world_cup_champions/MainActivity.java
@@ -0,0 +1,15 @@
+package com.world_cup_champions;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "world_cup_champions";
+  }
+}
