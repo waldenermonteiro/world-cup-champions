9e53401e34d4bff9f7fe3ee34f1a0c0fceccf1a6
Waldener Monteiro <jjrr019@gmail.com>
2021-02-07 23:38:45 -0300

    refactor(login): inheriting base repository settings and create signIn request

---
+++ src/pages/Auth/services/index.js
@@ -0,0 +1,16 @@
+import Base from '../../../services/base';
+
+export default new (class AuthService extends Base {
+  constructor() {
+    super('/login');
+  }
+  signIn = async ($data) => {
+    try {
+      const {data} = await this.service.post(`${this.api}/login`, $data);
+      console.log(data);
+      return data;
+    } catch (error) {
+      throw this.statusResponse(error);
+    }
+  };
+})();
