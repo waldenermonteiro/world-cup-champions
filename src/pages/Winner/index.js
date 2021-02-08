7f2b579d8b4d43950648cfcb6917d41b9a002e41
Waldener Monteiro <jjrr019@gmail.com>
2021-02-08 12:24:13 -0300

    refactor: list all winners

--- src/pages/Winner/index.js
+++ src/pages/Winner/index.js
@@ -1,13 +1,45 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Alert, SafeAreaView, ScrollView, View} from 'react-native';
+import {ListItem, Avatar} from 'react-native-elements';
+import Loader from '../../components/Loader';
 
-// import { Container } from './styles';
-
+import WinnerService from './services/index';
 const Winner = () => {
+  const [loading, setLoading] = useState(false);
+  const [winners, setWinners] = useState([]);
+  useEffect(() => {
+    async function loadStorageData() {
+      setLoading(true);
+      try {
+        const response = await WinnerService.list();
+        setWinners(response.winners);
+        setLoading(false);
+      } catch (error) {
+        Alert.alert(error.message);
+      }
+    }
+    loadStorageData();
+  }, []);
   return (
-    <View>
-      <Text>Winners</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <Loader loading={loading} />
+          {winners.map((item, i) => (
+            <ListItem bottomDivider key={i}>
+              <Avatar
+                source={require('../..//assets/icon.png')}
+                style={{width: 20, height: 40}}
+              />
+              <ListItem.Content>
+                <ListItem.Title>{item.country}</ListItem.Title>
+                <ListItem.Subtitle>{item.year}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          ))}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
